8b9208eefbc1f4c72fdfa5b0483ebd91781b3955
Vince Lee <vince.lee@polyhatsoftware.co.uk>
2021-08-09 14:34:01 +0100

    adds primitive types to about variables

--- koans/01_aboutVariables.test.ts
+++ koans/01_aboutVariables.test.ts
@@ -2,19 +2,27 @@ describe('About Variables', () => {
     const FILL_ME_IN = 'fill me in!'
 
     test(`const declares a variable`, () => {
-        const foo = 'bar'
+        const name = 'Morty'
 
-        expect(foo).toStrictEqual(FILL_ME_IN)
+        expect(name).toStrictEqual(FILL_ME_IN)
     })
 
     /* In most cases, we should avoid changing the value of (mutating) variables.
     Code that mutates state can be difficult to follow, and can cause unwanted
     side effects. */
-    test(`a const cannot be reassigned`, () => {
-            const foo = 'bar'
+    test(`a const containing a primitive types cannot be reassigned`, () => {
+        // the primitive types available:
+        const name = 'Rick'
+        expect(typeof(name)).toBe(FILL_ME_IN)
 
-            // try uncommenting the following line:
-            // foo = 'baz'
+        const isAPickle = true
+        expect(typeof(isAPickle)).toBe(FILL_ME_IN)
+
+        const totalAlternateUniversesRuined = 23932
+        expect(typeof(totalAlternateUniversesRuined)).toBe(FILL_ME_IN)
+
+        // try uncommenting the following line:
+        // foo = 'baz'
     })
 
     test(`if you really must re-assign a variable, you can use let`, () => {
